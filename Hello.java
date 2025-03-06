6d5c672818b8b699a82053363aa06499273113ad
BrunoHenrique000 <bhsilva2@minha.fag.edu.br>
2025-03-06 16:25:21 -0300

    add metodo holaquetal

--- Hello.java
+++ Hello.java
@@ -2,4 +2,12 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+<<<<<<< Updated upstream
+    public static void holaQueTal() {
+        System.out.println("Hola que tal");
+    }
+=======
+    
+>>>>>>> Stashed changes
 }
